e50b893a9acef9bc60d41019ddb2dd97ac93bd85
dinevsg <dinevsg@gmail.com>
2024-03-02 10:44:55 +0200

    update

---
+++ PostgreSQL/Exam Preparation I/11_ all_volunteers_in_a_department.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION fn_get_volunteers_count_from_department (
+	searched_volunteers_department VARCHAR(30)
+) RETURNS INT
+AS 
+$$
+BEGIN 
+RETURN (
+	SELECT 
+		COUNT(*) 
+	FROM 
+		volunteers AS v
+	JOIN 
+		volunteers_departments AS vd
+	ON 
+		v.department_id = vd.id
+	WHERE 
+		vd.department_name = searched_volunteers_department
+);
+END;
+$$
+LANGUAGE plpgsql;
+----------------------------------------------------<< TO CHECK >>--------------------------
+SELECT fn_get_volunteers_count_from_department ('Education program assistant')
