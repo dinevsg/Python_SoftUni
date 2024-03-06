7fb67987674c9486cd29dbfedaac536c2a77ea3a
dinevsg <dinevsg@gmail.com>
2024-03-06 11:16:43 +0200

    update

--- Django Basics/music_app/music_app/profiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from music_app.profiles.views import DeleteProfileView, DetailProfileView
-
-urlpatterns = (
-    path("delete/", DeleteProfileView.as_view(), name="delete_profile"),
-    path("details/", DetailProfileView.as_view(), name="details_profile")
-)
