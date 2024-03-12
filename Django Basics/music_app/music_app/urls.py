e5ff0d978a61ca68b9dcf4eaac39034304d819a1
S. Dinev <138312576+dinevsg@users.noreply.github.com>
2024-03-12 14:55:28 +0200

    Delete Django Basics/music_app directory

--- Django Basics/music_app/music_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("music_app.web.urls")),
-    path("album/", include("music_app.albums.urls")),
-    path("profiles/", include("music_app.profiles.urls")),
-]
